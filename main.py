a0ddc64f88a00c3c210c6fc2cdbd9b8b6e4c2b66
Róbert Čerňanský <openhs@tightmail.com>
2019-06-19 20:17:19 +0200

    Initial import.

---
+++ main.py
@@ -0,0 +1,18 @@
+# This Python file uses the following encoding: utf-8
+import sys
+import os
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtQuick import QQuickView
+from PyQt5.QtCore import QUrl
+
+
+
+if __name__ == "__main__":
+    app = QApplication([])
+
+    view = QQuickView()
+    currentDir = os.path.dirname(os.path.abspath(__file__))
+    view.setSource(QUrl(os.path.join(currentDir, "main.qml")))
+    view.show()
+
+    sys.exit(app.exec_())
